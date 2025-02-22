937e4212a9ff2d54da86cfe3873bb8d525d469c9
oluwaseyi.longe <oluwaseyi.longe@consorciomagalu.com.br>
2025-02-22 18:36:21 -0300

    refactor login page code

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import { useState } from "react";
-
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -26,13 +25,10 @@ import {
 } from "@/components/ui/form";
 import { useRouter } from "next/navigation";
 
-// Schema for login form
 const loginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8).max(50),
 });
-
-// Schema for registration form
 const registerSchema = z.object({
   firstName: z.string().min(1, "O nome é obrigatório"),
   lastName: z.string().min(1, "O sobrenome é obrigatório"),
@@ -41,15 +37,13 @@ const registerSchema = z.object({
   password: z.string().min(8, "A senha deve ter pelo menos 8 caracteres"),
 });
 
-// Define a union type for the form values
 type FormValues = z.infer<typeof loginSchema> | z.infer<typeof registerSchema>;
 
 export default function AuthForm() {
-  const [isRegistering, setIsRegistering] = useState(false); // Toggle between login and register
-  const [error, setError] = useState<string | null>(null); // State for error message
+  const [isRegistering, setIsRegistering] = useState(false);
+  const [error, setError] = useState<string | null>(null);
   const router = useRouter();
 
-  // Use the appropriate schema based on the form type
   const form = useForm<FormValues>({
     resolver: zodResolver(isRegistering ? registerSchema : loginSchema),
     defaultValues: {
@@ -61,62 +55,30 @@ export default function AuthForm() {
     },
   });
 
-  // Handle form submission
-  async function onSubmit(values: FormValues) {
+  const onSubmit = async (values: FormValues) => {
     try {
-      setError(null); // Clear any previous errors
-
+      setError(null);
       if (isRegistering) {
-        // Narrow the type to registration values
         const registrationValues = values as z.infer<typeof registerSchema>;
-
-        // Handle registration
-        const response = await axios.post("http://localhost:3005/professor", {
-          firstName: registrationValues.firstName,
-          lastName: registrationValues.lastName,
-          telephone: registrationValues.telephone,
-          email: registrationValues.email,
-          password: registrationValues.password,
-        });
-
-        console.log("Registration successful:", response.data);
-        setIsRegistering(false); // Switch back to login form after successful registration
+        await axios.post("http://localhost:3005/professor", registrationValues);
+        setIsRegistering(false);
       } else {
-        // Narrow the type to login values
         const loginValues = values as z.infer<typeof loginSchema>;
-
-        // Handle login
-        const response = await axios.post(
-          "http://localhost:3005/auth/sign-in",
-          {
-            email: loginValues.email,
-            password: loginValues.password,
-          }
-        );
-
-        const { accessToken, refreshToken } = response.data;
-
-        // Store tokens in local storage
+        const { accessToken, refreshToken } = (
+          await axios.post("http://localhost:3005/auth/sign-in", loginValues)
+        ).data;
         localStorage.setItem("token", accessToken);
         localStorage.setItem("refreshToken", refreshToken);
-
-        console.log("Login successful, tokens stored:", {
-          accessToken,
-          refreshToken,
-        });
         router.push("/manage-users");
       }
     } catch (error) {
-      console.error("Error:", error);
-
-      // Set error message based on the error
-      if (axios.isAxiosError(error)) {
-        setError(error.response?.data?.message || "Ocorreu um erro.");
-      } else {
-        setError("Ocorreu um erro inesperado.");
-      }
+      setError(
+        axios.isAxiosError(error)
+          ? error.response?.data?.message || "Ocorreu um erro."
+          : "Ocorreu um erro inesperado."
+      );
     }
-  }
+  };
 
   return (
     <main className="flex h-screen justify-center items-center">
@@ -133,118 +95,93 @@ export default function AuthForm() {
         </CardHeader>
         <CardContent>
           <div className="grid gap-4">
-            {/* Display error message if exists */}
             {error && (
               <div className="text-red-500 text-sm text-center">{error}</div>
             )}
-
             <Form {...form}>
               <form onSubmit={form.handleSubmit(onSubmit)}>
                 {isRegistering && (
                   <>
-                    <div className="grid gap-2">
-                      <FormField
-                        control={form.control}
-                        name="firstName"
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormLabel>Nome</FormLabel>
-                            <FormControl>
-                              <Input placeholder="Nome" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />
-                    </div>
-                    <div className="grid gap-2">
-                      <FormField
-                        control={form.control}
-                        name="lastName"
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormLabel>Sobrenome</FormLabel>
-                            <FormControl>
-                              <Input placeholder="Sobrenome" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />
-                    </div>
-                    <div className="grid gap-2">
-                      <FormField
-                        control={form.control}
-                        name="telephone"
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormLabel>Telefone</FormLabel>
-                            <FormControl>
-                              <Input placeholder="Telefone" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />
-                    </div>
+                    {["firstName", "lastName", "telephone"].map((field) => (
+                      <div key={field} className="grid gap-2">
+                        <FormField
+                          control={form.control}
+                          name={field}
+                          render={({ field: f }) => (
+                            <FormItem>
+                              <FormLabel>
+                                {field === "firstName"
+                                  ? "Nome"
+                                  : field === "lastName"
+                                  ? "Sobrenome"
+                                  : "Telefone"}
+                              </FormLabel>
+                              <FormControl>
+                                <Input
+                                  placeholder={
+                                    field === "firstName"
+                                      ? "Nome"
+                                      : field === "lastName"
+                                      ? "Sobrenome"
+                                      : "Telefone"
+                                  }
+                                  {...f}
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                      </div>
+                    ))}
                   </>
                 )}
-
-                <div className="grid gap-2">
-                  <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Email</FormLabel>
-                        <FormControl>
-                          <Input placeholder="Email" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-                <div className="grid gap-2">
-                  <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Senha</FormLabel>
-                        <FormControl>
-                          <Input
-                            type="password"
-                            placeholder="Senha"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <Button type="submit" className="w-full">
-                    {isRegistering ? "Registrar" : "Log in"}
-                  </Button>
-                </div>
+                {["email", "password"].map((field) => (
+                  <div key={field} className="grid gap-2">
+                    <FormField
+                      control={form.control}
+                      name={field}
+                      render={({ field: f }) => (
+                        <FormItem>
+                          <FormLabel>
+                            {field === "email" ? "Email" : "Senha"}
+                          </FormLabel>
+                          <FormControl>
+                            <Input
+                              type={field === "password" ? "password" : "text"}
+                              placeholder={
+                                field === "email" ? "Email" : "Senha"
+                              }
+                              {...f}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                ))}
+                <Button type="submit" className="w-full">
+                  {isRegistering ? "Registrar" : "Log in"}
+                </Button>
               </form>
             </Form>
-          </div>
-          <div className="mt-4 text-center text-sm">
-            {isRegistering
-              ? "Já possui uma conta? "
-              : "Ainda não possui uma conta? "}
-            <Link
-              href="#"
-              className="underline"
-              onClick={(e) => {
-                e.preventDefault();
-                setIsRegistering(!isRegistering);
-                form.reset(); // Reset form fields when switching
-              }}
-            >
-              {isRegistering ? "Log in" : "Registre-se"}
-            </Link>
+            <div className="mt-4 text-center text-sm">
+              {isRegistering
+                ? "Já possui uma conta? "
+                : "Ainda não possui uma conta? "}
+              <Link
+                href="#"
+                className="underline"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setIsRegistering(!isRegistering);
+                  form.reset();
+                }}
+              >
+                {isRegistering ? "Log in" : "Registre-se"}
+              </Link>
+            </div>
           </div>
         </CardContent>
       </Card>
